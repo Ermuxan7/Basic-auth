5d54793af3de4b46641670e597ff1d5eb59c3de5
Ermuxan <ermuxan747@gmail.com>
2025-05-11 22:23:01 +0300

    updated url

--- client/src/store/authStore.ts
+++ client/src/store/authStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL_PROD;
+const API_URL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
 type FormData = {
